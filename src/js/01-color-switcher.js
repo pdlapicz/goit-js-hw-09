a2ef58476b6140c6ecf7302bf6097adf3a1f0fee
Przemyslaw Daniel Lapicz <przemyslaw.lapicz@gmail.com>
2023-08-29 20:56:15 +0200

    add 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+window.onload = function () {
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
+  var timerId;
+
+  function start() {
+    timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }
+
+  function stop() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  }
+  startBtn.addEventListener('click', start);
+  stopBtn.addEventListener('click', stop);
+};
